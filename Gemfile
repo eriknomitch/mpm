09a3e91912764d0cd1ea25862c50493a49223f60
Erik Nomitch <enomitch@gmail.com>
2015-09-15 02:03:01 -0500

    adds requirements including Thor

--- Gemfile
+++ Gemfile
@@ -2,6 +2,12 @@ source "http://rubygems.org"
 # Add dependencies required to use your gem here.
 # Example:
 #   gem "activesupport", ">= 2.3.5"
+#gem "activesupport"
+#gem "recursive-open-struct"
+#gem "pidfile"
+#gem "os"
+gem "thor"
+gem "awesome_print"
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
--- lib/package.rb
+++ lib/package.rb
@@ -1,6 +1,16 @@
 # ================================================
 # PACKAGE ========================================
 # ================================================
+require "json"
+require "shellwords"
+require "yaml"
+require "recursive-open-struct"
+require "thor"
+require "thor/group"
+require "awesome_print"
+
+# FIX: Development only
+require "pry" if Gem::Specification::find_all_by_name("pry").any?
 
 # ------------------------------------------------
 # MODULE->PACKAGE --------------------------------
@@ -8,5 +18,8 @@
 module Package
   def self.main
     puts "ok test"
+  end
+
+  class PMProvisioner
   end
 end
