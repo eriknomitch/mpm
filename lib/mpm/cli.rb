08536ace223d4121397787e9356c88c959255163
Erik Nomitch <erik@nomitch.com>
2015-10-22 21:02:15 -0500

    + more commands

--- lib/mpm/cli.rb
+++ lib/mpm/cli.rb
@@ -14,15 +14,46 @@ module MPM
     package_name "mpm"
     
     # --------------------------------------------
+    # COMMAND->SEARCH ----------------------------
+    # --------------------------------------------
+    desc "search|--search PACKAGE", "Searches for a package in the list of available packages."
+    def search(package)
+      puts package
+    end
+
+    map "--search" => "search"
+    
+    # --------------------------------------------
+    # COMMAND->INSTALL ---------------------------
+    # --------------------------------------------
+    desc "install|--install PACKAGE", "Installs a package."
+    def install(package)
+      puts package
+    end
+
+    map "--install" => "install"
+    
+    # --------------------------------------------
     # COMMAND->UNINSTALL -------------------------
+    # --------------------------------------------
+    desc "uninstall|--uninstall PACKAGE", "Uninstalls a package."
+    def uninstall(package)
+      puts package
+    end
+
+    map "--uninstall" => "uninstall"
+    
+    # --------------------------------------------
+    # COMMAND->VERSION ---------------------------
     # --------------------------------------------
     desc "version|--version", "Displays the current version of notify-push"
     def version()
       puts "FIX"
-      #puts ::NotifyPush::VERSION
+      #puts ::MPM::VERSION
     end
 
     map "--version" => "version"
+
 
   end
 
