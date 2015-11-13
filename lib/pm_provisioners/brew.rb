bf3aff57524053432b7137d70db3059ea4d256a5
Erik Nomitch <enomitch@gmail.com>
2015-11-13 05:51:41 -0600

    splits out PMProvisioner definitions into their own files

---
+++ lib/pm_provisioners/brew.rb
@@ -0,0 +1,34 @@
+# ----------------------------------------------
+# DEFINE->PM-PROVISIONER->BREW -----------------
+# ----------------------------------------------
+PMProvisioner.define "brew", :osx do
+  install do |*packages|
+    ["install", *packages]
+  end
+  
+  uninstall do |*packages|
+    ["uninstall", *packages]
+  end
+
+  search do |package|
+    ["search", package]
+  end
+
+  search_installed do |package|
+    #executable "apt-cache" 
+    ["search", package, "|", "grep", "-i", package]
+  end
+
+  list do
+    ["list"]
+  end
+
+  update do
+    ["update"]
+  end
+  
+  info do |package|
+    ["info", package]
+  end
+end
+
