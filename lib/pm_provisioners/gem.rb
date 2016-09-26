082c07e174e5fa5465782eef9853346a11227502
Erik Nomitch <enomitch@gmail.com>
2016-09-26 06:09:43 -0500

    RubyGem

---
+++ lib/pm_provisioners/gem.rb
@@ -0,0 +1,23 @@
+module MPM
+  class RubyGems < Thor
+    desc "uninstall...", "uninstall......"
+    def uninstall(*packages)
+      system "gem uninstall #{packages.join(" ")}"
+    end
+
+    desc "install...", "install......"
+    def install(*packages)
+      system "gem install #{packages.join(" ")}"
+    end
+
+    desc "list...", "list......"
+    def list(*packages)
+      system "gem list"
+    end
+  end
+
+  class CLI < Thor
+    desc "/gem COMMAND...ARGS", "Meta wrapper for 'gem'."
+    subcommand "/gem", RubyGems
+  end
+end
