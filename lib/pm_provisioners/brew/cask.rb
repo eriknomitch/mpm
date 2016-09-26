357c9ea6544c031a82e397918c4dd943b165194c
Erik Nomitch <enomitch@gmail.com>
2016-09-26 06:04:20 -0500

    OS X check

--- lib/pm_provisioners/brew/cask.rb
+++ lib/pm_provisioners/brew/cask.rb
@@ -1,23 +1,25 @@
 module MPM
-  class BrewCask < Thor
-    desc "uninstall...", "uninstall......"
-    def uninstall(*packages)
-      system "brew cask uninstall #{packages.join(" ")}"
+  if Utility.get_os == :osx
+    class BrewCask < Thor
+      desc "uninstall...", "uninstall......"
+      def uninstall(*packages)
+        system "brew cask uninstall #{packages.join(" ")}"
+      end
+
+      desc "install...", "install......"
+      def install(*packages)
+        system "brew cask install #{packages.join(" ")}"
+      end
+      
+      desc "list...", "list......"
+      def list(*packages)
+        system "brew cask list"
+      end
     end
 
-    desc "install...", "install......"
-    def install(*packages)
-      system "brew cask install #{packages.join(" ")}"
+    class CLI < Thor
+      desc "/cask COMMAND...ARGS", "Meta wrapper for 'brew cask'."
+      subcommand "/cask", BrewCask
     end
-    
-    desc "list...", "list......"
-    def list(*packages)
-      system "brew cask list"
-    end
-  end
-
-  class CLI < Thor
-    desc "/cask COMMAND...ARGS", "Meta wrapper for 'brew cask'."
-    subcommand "/cask", BrewCask
   end
 end
